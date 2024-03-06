c20f30d6c40ee59d1c38943be11de3dd5c972cb5
シム ウク <astralkaiser89@gmail.com>
2024-03-06 20:09:33 +0900

    vue js code clean and view create h2 setting edit

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,21 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/write">コンテンツ作成</RouterLink>
+    </nav>
   </header>
 
   <RouterView />
 </template>
-
 <style>
-
 </style>
--- front/src/main.ts
+++ front/src/main.ts
@@ -6,9 +6,15 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import elementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+
+import "bootstrap/dist/css/bootstrap-utilities.css"
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(elementPlus)
 
 app.mount('#app')
